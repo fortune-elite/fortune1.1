9c29ebb3848391f5b7689437cd17a017da604ad5
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-10-07 23:54:13 +0100

    working

--- components/WhyChooseUs.js
+++ components/WhyChooseUs.js
@@ -4,7 +4,7 @@ const WhyChooseUs = () => {
             <h1 className="font-bold text-4xl ml-40">Why Choose Us</h1>
             <p className="text-sm py-2 px-16 sm:px-2">We help you save money without putting forth a lot of effort with computerized savings</p>
 
-            <div className=" flex flex-col space-x-12  md:flex-col  lg:flex-row pt-0 px-4 md:pt-5 lg:pt-5">
+            <div className=" flex  space-x-12  md:flex-col  lg:flex-row pt-0 px-4 md:pt-5 lg:pt-5">
                 <div className="bg-blue-800 h-36 w-44 rounded-sm shadow-sm max-w-sm min-h-full overflow-hidden">
                     <p className="text-sm text-white pt-10 px-5">Create an account with us within 2-3 minutes for FREE</p>
                 </div>
