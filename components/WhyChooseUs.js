6a9d5ab4d99a510876c24aa03d654be5f170c63b
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-10-08 00:11:05 +0100

    working

--- components/WhyChooseUs.js
+++ components/WhyChooseUs.js
@@ -4,14 +4,14 @@ const WhyChooseUs = () => {
             <h1 className="font-bold py-2 text-4xl md:ml-40 lg:ml-40 sm:ml-10 " >Why Choose Us</h1>
             <p className="text-sm py-2 px-16 sm:px-2">We help you save money without putting forth a lot of effort with computerized savings</p>
 
-            <div className=" flex  md:flex-col space-x-12 sm: space-y-8  lg:flex-row pt-0 px-4 sm:flex flex-col sm:px-1 md:pt-5 lg:pt-5">
-                <div className="bg-blue-800 h-36 w-44 rounded-sm shadow-sm max-w-sm min-h-full overflow-hidden">
+            <div className=" flex  md:flex-row space-x-12 sm:space-y-8  lg:flex-row pt-0 px-4 sm:flex flex-col sm:px-1 md:pt-5 lg:pt-5">
+                <div className="bg-blue-800 h-36 w-44 sm:w-60 sm:h-60 rounded-sm shadow-sm max-w-sm min-h-full overflow-hidden">
                     <p className="text-sm text-white pt-10 px-5">Create an account with us within 2-3 minutes for FREE</p>
                 </div>
-                <div className="bg-blue-500 h-36 w-44 rounded-md shadow-sm max-w-sm min-h-full overflow-hidden">
+                <div className="bg-blue-500 h-36 w-44  sm:w-60 sm:h-60 rounded-md shadow-sm max-w-sm min-h-full overflow-hidden">
                     <p className="text-sm text-white pt-10 px-5">Save on the go to reach your target goals</p>
                 </div>
-                <div className="bg-gray-800 h-36 w-44 rounded-sm shadow-sm max-w-sm min-h-full overflow-hidden">
+                <div className="bg-gray-800 h-36 w-44 sm:w-60 sm:h-60 rounded-sm shadow-sm max-w-sm min-h-full overflow-hidden">
                     <p className="text-sm text-white pt-10 px-5">Take out a loan against your escrow account</p>
                 </div>
             </div>
