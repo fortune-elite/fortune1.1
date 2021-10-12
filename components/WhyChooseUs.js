db0baa9d2650a4350b224fe58aa3b991c3ae3804
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-10-12 08:24:49 +0100

    working

--- components/WhyChooseUs.js
+++ components/WhyChooseUs.js
@@ -6,8 +6,8 @@ import { FaHandHoldingUsd } from "react-icons/fa";
 const WhyChooseUs = () => {
     return ( 
         <div className= " flex-row items-center  md:flex-row  lg:flex-row pt-0  md:pt-10 lg:pt-10 ">
-            <h1 className="font-bold py-2 text-4xl ml-10 md:ml-40 lg:ml-40  " >Why Choose Us</h1>
-            <p className="text-sm py-2 px-16 sm:px-2">We help you save money without putting forth a lot of effort with computerized savings</p>
+            <h1 className="font-bold py-2 text-4xl ml-10 md:ml-52   " >Why Choose Us</h1>
+            <p className="text-sm py-2 px-2 ml-0 md:ml-28 ">We help you save money without putting forth a lot of effort with computerized savings</p>
 
             <div className=" flex flex-col md:flex-row px-14 space-y-8 space-x-0 md:space-y-0 md:space-x-8  lg:flex-row pt-0  md:pt-5 lg:pt-5">
                 <div className="bg-blue-800 h-52 w-60 md:w-48 rounded-lg shadow-sm overflow-hidden">
